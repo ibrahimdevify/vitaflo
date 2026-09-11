0649cc6f53aac2e712438a3a8ae57a6f49c29cb1
bilaldevify <150142486+bilaldevify@users.noreply.github.com>
2026-09-11 17:40:48 +0500

    Update visible_clinician.js

--- api/src/helpers/visible_clinician.js
+++ api/src/helpers/visible_clinician.js
@@ -1,16 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-
 const getVisibleClinicianIds = async (user) => {
-  if (
-    (user.ut_id_fk === 3 || user.ut_id_fk === 6) &&
-    user.us_id_fk === 1
-  ) {
+  if (user.ut_id_fk === 3 || user.ut_id_fk === 6) {
     return [user.user_id];
   }
 
   return [];
 };
-
 module.exports = { getVisibleClinicianIds };
